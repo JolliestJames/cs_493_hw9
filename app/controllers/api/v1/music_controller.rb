8e341f4f00a3d2d711efc5ac96641843d0634d3e
JolliestJames <cloudismydog@gmail.com>
2019-02-09 15:59:33 -0800

    Use MusicService to return mapped song contents as JSON

--- app/controllers/api/v1/music_controller.rb
+++ app/controllers/api/v1/music_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MusicController < ActionController::API
       def index
-        render json: { music: 'Someday there will be music here' }, status: :ok
+        render json: { music: MusicService.encode_songs }, status: :ok
       end
     end
   end
