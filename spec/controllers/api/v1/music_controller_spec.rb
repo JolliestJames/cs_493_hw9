59bd2aa528e5d2ab9b14b55af097bf0c3d30f08c
JolliestJames <cloudismydog@gmail.com>
2019-02-10 16:33:12 -0800

    Update spec

--- spec/controllers/api/v1/music_controller_spec.rb
+++ spec/controllers/api/v1/music_controller_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::MusicController, type: :controller do
   describe 'GET #index' do
-    let(:expected) { { music: {'artist/album/song': 'some encoded song'} }.to_json }
+    let(:expected) { { music: { 'artist': {'album': {'song': 'url' } } } } }
 
     before do
-      allow(MusicService).to receive(:encode_songs).and_return({'artist/album/song': 'some encoded song'})
+      allow(MusicService).to receive(:music).and_return(expected[:music])
     end
 
-    specify { expect(get(:index).body).to eq(expected) }
+    specify { expect(get(:index).body).to eq(expected.to_json) }
   end
 end
