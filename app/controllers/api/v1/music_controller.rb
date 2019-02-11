fe584750dbc062c627731922007092b64b4d8fe7
JolliestJames <cloudismydog@gmail.com>
2019-02-10 16:33:06 -0800

    Return results from MusicService.music instead of MusicService.encode_songs

--- app/controllers/api/v1/music_controller.rb
+++ app/controllers/api/v1/music_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MusicController < ActionController::API
       def index
-        render json: { music: MusicService.encode_songs }, status: :ok
+        render json: { music: MusicService.music }, status: :ok
       end
     end
   end
