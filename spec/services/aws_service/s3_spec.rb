d456b8a4c99892b4fde57dc91daa85a65b2f8d1a
JolliestJames <cloudismydog@gmail.com>
2019-02-11 00:57:33 -0800

    Update spec

--- spec/services/aws_service/s3_spec.rb
+++ spec/services/aws_service/s3_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe AwsService::S3 do
   end
 
   describe '.put_object' do
-    let(:object) { 'spec/fixtures/test.txt' }
-    let(:path) { 'spec/fixtures/test.txt' }
+    let(:object) { 'song.mp3' }
+    let(:path) { 'artic_monkeys/am/do_i_wanna_know?' }
 
     it 'returns a Seahorse::Client::Response' do
       expect(described_class.put_object(object, path)).to be_a(Seahorse::Client::Response)
@@ -19,13 +19,13 @@ RSpec.describe AwsService::S3 do
   end
 
   describe '.object' do
-    let(:key) { 'spec/fixtures/test.txt' }
+    let(:key) { 'artic_monkeys/am/do_i_wanna_know?' }
 
     specify { expect(described_class.object(key)).to be_a(Seahorse::Client::Response) }
   end
 
   describe '.object_url' do
-    let(:key) { 'spec/fixtures/test.txt' }
+    let(:key) { 'artic_monkeys/am/do_i_wanna_know?' }
 
     specify { expect(described_class.object_url(key)).to be_a(String) }
   end
