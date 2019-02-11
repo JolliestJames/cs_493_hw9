1a9fa933da8dfd1ed4396385afdc0877a7c892b9
JolliestJames <cloudismydog@gmail.com>
2019-02-10 16:32:06 -0800

    Cover .music, clean up

--- spec/services/music_service_spec.rb
+++ spec/services/music_service_spec.rb
@@ -13,16 +13,25 @@ RSpec.describe MusicService do
       { 'spec/fixtures/test.txt': Base64.encode64(File.open('spec/fixtures/test.txt').read) }
     end
 
-    specify do
-      expect(described_class.encode_songs).to eq expected
-    end
+    specify { expect(described_class.encode_songs).to eq expected }
   end
 
   describe '.songs' do
     let(:expected) { [ { "spec/fixtures/test.txt": "some song\n" } ] }
 
-    specify do
-      expect(described_class.songs).to eq(expected)
+    specify { expect(described_class.songs).to eq(expected) }
+  end
+
+  describe '.music' do
+    let(:expected) do
+      { artist: { album: { 'song.mp3': 'url' } } }
     end
+
+    before do
+      allow(described_class).to receive(:song_keys).and_return(['artist/album/song.mp3'])
+      allow(AwsService::S3).to receive(:object_url).and_return('url')
+    end
+
+    specify { expect(described_class.music).to eq(expected) }
   end
 end
