e07831aae20e4539d628998b65974c5d6bad8e40
JolliestJames <cloudismydog@gmail.com>
2019-02-16 01:55:38 -0800

    Add new music controller spec

---
+++ spec/controllers/api/v2/music_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Api::V2::MusicController, type: :controller do
+  describe 'GET #genres' do
+    
+  end
+
+  describe 'GET #artists' do
+    
+  end
+
+  describe 'GET #albums' do
+    
+  end
+
+  describe 'GET #songs' do
+    
+  end
+
+  describe 'GET #song' do
+    
+  end
+end
