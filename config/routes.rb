c3908dd37ace5f863547213eb5f7f98868fa4e70
JolliestJames <cloudismydog@gmail.com>
2019-02-16 01:55:55 -0800

    Add routing for new music controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'genres', action: :genres, controller: 'api/v2/music'
+  get 'artists', action: :artists, controller: 'api/v2/music'
+  get 'albums', action: :albums, controller: 'api/v2/music'
+  get 'songs', action: :songs, controller: 'api/v2/music'
+  get 'song', action: :song, controller: 'api/v2/music'
+
   namespace :api do
     namespace :v1 do
       get 'music', action: :index, controller: 'music'
