c37a5db0121ea9a2087f72adaf2a884a80654d09
JolliestJames <cloudismydog@gmail.com>
2019-02-16 22:03:48 -0800

    Use V1 of the music service

--- app/controllers/api/v1/music_controller.rb
+++ app/controllers/api/v1/music_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MusicController < ActionController::API
       def index
-        render json: { music: MusicService.music }, status: :ok
+        render json: { music: MusicService::V1.music }, status: :ok
       end
     end
   end
