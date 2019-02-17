8b865165c71c242d1cc6fe548994ad52aa5846e0
JolliestJames <cloudismydog@gmail.com>
2019-02-16 22:15:55 -0800

    Update spec, cover .jsonify

--- spec/services/music_service_spec.rb
+++ spec/services/music_service_spec.rb
@@ -2,45 +2,19 @@ require 'rails_helper'
 require 'base64'
 
 RSpec.describe MusicService do
-  describe '.encode_song' do
-    let(:expected) { Base64.encode64('a song\n') }
-
-    specify { expect(described_class.encode_song('a song\n')).to eq(expected) }
-  end
-
-  describe '.encode_songs' do
+  describe '.jsonify' do
+    let(:key) { 'artist/album/song' }
+    let(:url) { 'https://s3.amazonaws.com/neat-bucket/artist/album/song.mp3' }
     let(:expected) do
-      { 'song': Base64.encode64('file') }
+      {
+        "artist": {
+          "album": {
+            "song": url
+          }
+        }
+      }
     end
 
-    before do
-      allow(described_class).to receive(:songs).and_return([{song: 'file'}])
-    end
-
-    specify { expect(described_class.encode_songs).to eq expected }
-  end
-
-  describe '.songs' do
-    let(:expected) { [ { 'artic_monkeys/am/do_i_wanna_know?': 'song contents' } ] }
-
-    before do
-      allow(described_class).to receive(:song_keys).and_return(['artic_monkeys/am/do_i_wanna_know?'])
-      allow(AwsService::S3).to receive(:read).and_return('song contents')
-    end
-
-    specify { expect(described_class.songs).to eq(expected) }
-  end
-
-  describe '.music' do
-    let(:expected) do
-      { artist: { album: { 'song.mp3': 'url' } } }
-    end
-
-    before do
-      allow(described_class).to receive(:song_keys).and_return(['artist/album/song.mp3'])
-      allow(AwsService::S3).to receive(:object_url).and_return('url')
-    end
-
-    specify { expect(described_class.music).to eq(expected) }
+    specify { expect(described_class.jsonify(key, url)).to eq(expected) }
   end
 end
