9e4451dedc28cf71d0b18a693871df6a92f063cf
JolliestJames <cloudismydog@gmail.com>
2019-02-17 15:46:38 -0800

    Update spec, work in progress

--- spec/controllers/api/v2/music_controller_spec.rb
+++ spec/controllers/api/v2/music_controller_spec.rb
@@ -2,7 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Api::V2::MusicController, type: :controller do
   describe 'GET #genres' do
-    
+    let(:expected) { { genres: ['genre_1', 'genre_2', 'genre_3'] } }
+
+    before do
+      allow(MusicService::V2).to receive(:genres).and_return(expected[:genres])
+    end
+
+    specify { expect(get(:genres).body).to eq(expected.to_json) }
+    specify { expect(get(:genres)).to be_successful }
   end
 
   describe 'GET #artists' do
