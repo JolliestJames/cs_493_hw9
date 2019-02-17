8ca395504bf5cb79c71de8d245646d2f3299a676
JolliestJames <cloudismydog@gmail.com>
2019-02-17 15:46:20 -0800

    Implement MusicService::V2 methods

--- app/services/music_service/v2.rb
+++ app/services/music_service/v2.rb
@@ -1,20 +1,30 @@
 module MusicService
   class V2
     class << self
-      def genre
-
+      def genres
+        AwsService::DynamoDB.scan('genre').map { |i| i['genre'] }.to_set.to_a
       end
 
-      def artist
-
+      def genre(key)
+        AwsService::DynamoDB.query(genre: key).each.map { |item| item['artist'] }.to_set.to_a
       end
 
-      def album
-
+      def artist(key)
+        genres.map do |g|
+          AwsService::DynamoDB.query(genre: g, artist: key, method: :artist)
+        end.first.each.map { |item| item['album'] }.to_set.to_a
       end
 
-      def song
+      def album(key)
+        genres.map do |g|
+          AwsService::DynamoDB.query(genre: g, album: key, method: :album)
+        end.first.each.map { |item| item['song'] }.to_set.to_a
+      end
 
+      def song(key)
+        genres.map do |g|
+          AwsService::DynamoDB.query(genre: g, song: key, method: :song)
+        end.first.first['s3_location']
       end
     end
   end
