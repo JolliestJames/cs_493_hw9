eed2ca9da5e9b7010988bee6b2efea6000b5e8f5
JolliestJames <cloudismydog@gmail.com>
2019-02-18 01:19:03 -0800

    Fill out spec

--- spec/controllers/api/v2/music_controller_spec.rb
+++ spec/controllers/api/v2/music_controller_spec.rb
@@ -13,18 +13,66 @@ RSpec.describe Api::V2::MusicController, type: :controller do
   end
 
   describe 'GET #artists' do
-    
+    let(:expected) { { artists: ['artist_1', 'artist_2', 'artist_3'] } }
+
+    before do
+      allow(MusicService::V2).to receive(:genre).and_return(expected[:artists])
+    end
+
+    it 'returns a list of artists belonging to a genre' do
+      get :artists, params: { genre: 'genre' }
+
+      expect(response.body).to eq(expected.to_json)
+    end
+
+    specify { expect(get(:artists, params: { genre: 'genre' })).to be_successful }
   end
 
   describe 'GET #albums' do
-    
+    let(:expected) { { albums: ['album_1', 'album_2', 'album_3'] } }
+
+    before do
+      allow(MusicService::V2).to receive(:artist).and_return(expected[:albums])
+    end
+
+    it 'returns a list of albums belonging to an artist' do
+      get :albums, params: { artist: 'artist' }
+
+      expect(response.body).to eq(expected.to_json)
+    end
+
+    specify { expect(get(:albums, params: { artist: 'artist' })).to be_successful }
   end
 
   describe 'GET #songs' do
-    
+    let(:expected) { { songs: ['song_1', 'song_2', 'song_3'] } }
+
+    before do
+      allow(MusicService::V2).to receive(:album).and_return(expected[:songs])
+    end
+
+    it 'returns a list of songs belonging to an album' do
+      get :songs, params: { album: 'album' }
+
+      expect(response.body).to eq(expected.to_json)
+    end
+
+    specify { expect(get(:songs, params: { album: 'album' })).to be_successful }
   end
 
   describe 'GET #song' do
-    
+    let(:expected) { { url: 'url' } }
+
+    before do
+      allow(MusicService::V2).to receive(:song).and_return(expected[:url])
+    end
+
+    it 'returns the S3 url of a song' do
+      get :song, params: { song: 'song' }
+
+      expect(response.body).to eq(expected.to_json)
+    end
+
+    specify { expect(get(:song, params: { song: 'song' })).to be_successful }
   end
 end
